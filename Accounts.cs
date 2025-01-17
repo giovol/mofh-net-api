7007661de425da4ceb2ceb1e3512a5ee66b916ce
Giovanni Voltan <187798689+giovol@users.noreply.github.com>
2025-01-17 21:32:30 +0100

    Code cleanup

--- Accounts.cs
+++ Accounts.cs
@@ -1,12 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
-using System.Diagnostics;
 
 
 namespace MOFH_api
