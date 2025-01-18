41d6e95c86f41b8861327ecf8e1c92d830841a37
Giovanni Voltan <187798689+giovol@users.noreply.github.com>
2025-01-18 19:46:47 +0100

    Add Tickets.cs documentation

--- source/xml/Tickets.cs
+++ source/xml/Tickets.cs
@@ -5,6 +5,12 @@ namespace mofh.xml
 {
     public static class Tickets
     {
+        /// <summary>
+        /// Create a new ticket (https://api.myownfreehost.net/XML/tickets/create)
+        /// </summary>
+        /// <param name="apiUsername">API username.</param>
+        /// <param name="apiPassword">API password.</param>
+        /// <param name="domain">Domain associated with the ticket.</param>
         public static async void Create(string apiUsername, string apiPassword, string domain)
         {
             using (var httpClient = new HttpClient())
@@ -21,6 +27,13 @@ namespace mofh.xml
                 }
             }
         }
+        /// <summary>
+        /// Create a new reply from user (https://api.myownfreehost.net/XML/tickets/user-reply)
+        /// </summary>
+        /// <param name="apiUsername">API username.</param>
+        /// <param name="apiPassword">API password.</param>
+        /// <param name="ticketId">Ticket ID.</param>
+        /// <param name="domain">Domain associated with the ticket.</param>
         public static async void UserReply(string apiUsername, string apiPassword, string ticketId, string domain)
         {
             using (var httpClient = new HttpClient())
